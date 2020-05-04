4ad4f9d71f812f330d7601108a83c3542f091c0c
Sunil Gautam <gautam.sunil01@gmail.com>
2020-05-04 22:58:55 +0530

    class defined for library

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = {};
+const Redis = require('ioredis');
+
+class TaggedCache {
+  constructor(options = {}) {
+    const redisOption = options.redis || {};
+    /**
+     * if redis instance is passed as option
+     * then no need to initialize new instance
+     */
+    this.redis =
+      redisOption instanceof Redis ? redisOption : new Redis(options.redis);
+  }
+}
+
+module.exports = TaggedCache;
