bba5526d12d09176c42b8795add320fc75abba80
Sunil Gautam <gautam.sunil01@gmail.com>
2020-05-05 23:20:39 +0530

    put cache item with tags added

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const Redis = require('ioredis');
 
-class TaggedCache {
-  constructor(options = {}) {
-    const redisOption = options.redis || {};
-    /**
-     * if redis instance is passed as option
-     * then no need to initialize new instance
-     */
-    this.redis =
-      redisOption instanceof Redis ? redisOption : new Redis(options.redis);
-  }
-}
+module.exports = (options = {}) => {
+  const redisOption = options.redis || {};
+  /**
+   * if redis instance is passed as option
+   * then no need to initialize new instance
+   */
+  const redis =
+    redisOption instanceof Redis ? redisOption : new Redis(options.redis);
 
-module.exports = TaggedCache;
+  return {
+    async put(key, data, tags, ttl) {
+      if (tags.length) {
+        // cache data with tag
+        const multi = redis.multi();
+        multi.setex(key, ttl, data);
+        tags.forEach((tag) => {
+          multi.sadd(`tag:${tag}`, key);
+        });
+        await multi.exec();
+      } else {
+        // cache data without tag
+        await redis.setex(key, ttl, data);
+      }
+    },
+  };
+};
